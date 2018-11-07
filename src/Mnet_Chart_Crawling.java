8eb02109fd6b069335427cde32c09e5ab0e0bd6c
dudgns3tp <40652160+dudgns3tp@users.noreply.github.com>
2018-11-07 14:15:40 +0900

    Update Mnet_Chart_Crawling.java
    Input/ 제목,  탐색 시작날짜, 탐색 종료날짜 
    Output/ 날짜 제목 가수 순위 
    
    DB 연동 후 재업로드 예정

--- src/Mnet_Chart_Crawling.java
+++ src/Mnet_Chart_Crawling.java
@@ -3,45 +3,88 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
- 
-public class JsoupTest {
- 
-    /*
-     *  Document 클래스 : 연결해서 얻어온 HTML 전체 문서
-     *  Element 클래스  : Documnet의 HTML 요소
-     *  Elements 클래스 : Element가 모인 자료형
-     */
-     
-    public static void main(String[] args) {
-        // Jsoup를 이용해서 엠넷차트
-        String url = "http://www.mnet.com/chart/TOP100/2018011013";
-        Document doc = null;
-         
-        try {
-            doc = Jsoup.connect(url).get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-         
-        // 주요 차트를 나오는 태그를 찾아서 가져오도록 한다.
-        Elements element =doc.select("div.MMLTable");
+import java.util.Scanner;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
-         
-        // 1. 헤더 부분의 제목을 가져온다.
-        String title = element.select("caption").text().substring(0, 10);
+public class date {
 
- 
-        System.out.println("============================================================");
-        System.out.println(title);
-        System.out.println("============================================================");
-         
-        int rank=1;
-       for(Element el : element.select("div.MMLITitleSong_BOX")) {    // 하위 차트들을 for문 돌면서 출력
-            System.out.println(rank+" "+el.text());
-            rank++;
-        }
+	public static String doDateAdd(String dates) throws ParseException { // 날짜 계산
+		Calendar cal = new GregorianCalendar(Locale.KOREA); // cal 객체생성
+		SimpleDateFormat fm = new SimpleDateFormat("yyyyMMddHH");// Dateform 생성
 
-        System.out.println("============================================================");
-    }
- 
+		Date date = fm.parse(dates);// 입력받은 문자열 date를 Date형식으료 변환
+		cal.setTime(date);
+		cal.add(Calendar.DAY_OF_YEAR, 1); // 하루를 더한다.
+		String strDate = fm.format(cal.getTime());
+		return strDate;
+	}
+
+	public static void main(String[] args) throws ParseException {
+		// Jsoup를 이용해서 엠넷차트
+		Scanner sc = new Scanner(System.in);
+		System.out.println("노래 제목 검색");
+		String Song_name = sc.nextLine();
+		System.out.println("음원 탐색 기간 설정  (언제부터? ex) yyyyMMDDHH)");
+		String to_Date = sc.next();
+		System.out.println("음원 탐색 기간 설정 (언제까지? ex) yyyyMMDDHH");
+		String from_Date = sc.next();
+		String Artist ="";
+		String Title ="";
+		do {
+			String url = "http://www.mnet.com/chart/TOP100/" + to_Date;
+			String url2 = "http://www.mnet.com/chart/TOP100/" + to_Date +"?pNum=2";
+			Document doc = null;
+			Document doc2 = null;
+			int cnt = 1; // 음원 차트 순위 카운팅
+
+			try { // Jsoup
+				doc = Jsoup.connect(url).get();
+				doc2 = Jsoup.connect(url2).get();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			boolean is_song = false; // 해당 노래가 있는지 체크
+
+			// 주요 차트를 나오는 태그를 찾아서 가져오도록 한다.
+			Elements element = doc.select("div.MMLTable");
+			Elements element2 = doc2.select("div.MMLTable");
+
+			for (Element el : element.select("div.MMLITitle_Box.info")) {
+				Title =el.select("a.MMLI_Song").text();
+				Artist = el.select("a.MMLIInfo_Artist").text();
+				if (Song_name.contains(Title)) {
+					System.out.println("날짜:" + to_Date + " 제목:" + Title + " 가수 :"+ Artist +" 순위:" + cnt + "위");
+					is_song = true;
+					break;
+				}
+				cnt++;
+			}
+			if (!is_song) {
+				for (Element el : element2.select("div.MMLITitle_Box.info")) {
+					Title =el.select("a.MMLI_Song").text();
+					Artist = el.select("a.MMLIInfo_Artist").text();
+					if (Song_name.contains(Title)) {
+						System.out.println("날짜:" + to_Date + " 제목:" + Title + " 가수 :"+ Artist +" 순위:" + cnt + "위");
+						is_song = true;
+						break;
+					}
+					cnt++;
+
+				}
+				if(!is_song) {
+					System.out.println("날짜:" + to_Date + " 제목: " + Song_name + " 해당 노래가 차트에 없습니다.");
+				}
+			}
+					
+
+			to_Date = doDateAdd(to_Date);
+
+		} while (!(to_Date.equals(from_Date)));
+		sc.close();
+	}
 }
