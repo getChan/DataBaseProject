dce98bece82f7bca02de4b472f467d1245f6213c
getChan <201401438@inu.ac.kr>
2018-11-23 15:53:57 +0900

    test model

---
+++ src/dbQuery.py
@@ -0,0 +1,30 @@
+import pymysql
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# 쿼리를 입력하면 db에 있는 데이터를 pandas DataFrame형태로 반환
+def dbQuery(query):
+    
+    conn = pymysql.connect(host='zuzak.cvqcrkck1aqg.us-east-1.rds.amazonaws.com', user='getChan', password='cksdl951!!',db='zuzak', charset='euckr')
+    q = query
+    try:
+        with conn.cursor() as cursor:
+            df = pd.read_sql(q, con=conn)
+            cursor.fetchall()
+    finally:
+        conn.close()
+        return df
+
+# DataFrame 전처리 함수.
+def dfFilter(dataFrame):
+    df = dataFrame.set_index("YYMMDD")
+    df.ranking =pd.to_numeric(df.ranking)
+
+    return df
+
+# dataFrame 시각화
+def dfPlot(dataFrame):
+    dataFrame.plot()
+    plt.gca().invert_yaxis()
+    plt.ylabel('ranking')
+    plt.show()
